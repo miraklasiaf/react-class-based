f08308608c26e6996fa49a9b404d0920fd19073e
Faisal Karim <faisalkarim96@gmail.com>
2020-02-25 20:58:49 +0700

    Update some stuff

--- src/App.js
+++ src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './assets/App.css';
+import Layout from './components/Layout/Layout'
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder'
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      userInput: ''
-    }
-  }
-
   render() {
     return (
       <div className="text-center">
-        <header className="bg-gray-600 m-6 p-6 rounded shadow-lg hover:bg-red-600">
-          <img
-            src={logo}
-            className="h-32 w-32 rounded-full mx-auto App-logo"
-            alt="logo"
-          />
-          <h1 className="text-white text-3xl">Welcome to React</h1>
-        </header>
-        <p className="text-base">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
